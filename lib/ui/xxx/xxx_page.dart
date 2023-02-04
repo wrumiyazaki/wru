99e6c5ba5024f8e6124abe2b9dd2c62a0a38439d
shunsuke-tamura <wwwxx4869@gmail.com>
2023-02-04 17:11:42 +0900

    update: pageにテーマ適応

--- lib/ui/xxx/xxx_page.dart
+++ lib/ui/xxx/xxx_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_study_mvvm/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_study_mvvm/ui/theme/app_text_theme.dart';
+import 'package:flutter_study_mvvm/ui/theme/app_theme.dart';
 import 'package:flutter_study_mvvm/ui/xxx/xxx_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -9,6 +11,7 @@ class XXXPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appThemeProvider);
     final state = ref.watch(xxxViewModelProvider);
     final viewModel = ref.watch(xxxViewModelProvider.notifier);
 
@@ -23,9 +26,12 @@ class XXXPage extends HookConsumerWidget {
                   Assets.img.flutterIcon.image(width: 200),
                   Text(
                     L10n.of(context)!.hello,
-                    style: Theme.of(context).textTheme.headline4,
+                    style: theme.textTheme.h70.bold(),
                   ),
                   ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: theme.appColors.primary,
+                          onPrimary: theme.appColors.onPrimary),
                       onPressed: viewModel.increment,
                       child: Text(data.count.toString()))
                 ],
