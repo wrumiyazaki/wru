07eba38f5273d7129679b87001f6a6b270d99483
shunsuke-tamura <wwwxx4869@gmail.com>
2023-02-05 06:27:43 +0900

    update: devビルドの時はtoastを表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'dart:async';
 
+import 'package:enum_to_string/enum_to_string.dart';
 import 'package:fimber/fimber.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_study_mvvm/foundation/constants.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   if (!kReleaseMode) {
     Fimber.plantTree(DebugTree());
   } else {
     debugPrint = (message, {wrapWidth}) {};
   }
+
+  if (Constants.flavor == Flavor.dev) {
+    Fluttertoast.showToast(
+        msg: "flavor: ${EnumToString.convertToString(Constants.flavor)}");
+  }
+
   runZonedGuarded(() => runApp(const ProviderScope(child: MyApp())),
       (error, stack) {
     Fimber.e(error.toString());
