b66adb4e84e6e853467daa1f0e31e9f242e0c9f8
shunsuke-tamura <wwwxx4869@gmail.com>
2023-02-04 10:24:06 +0900

    update: repositoryを使って実装

--- lib/ui/xxx/xxx_view_model.dart
+++ lib/ui/xxx/xxx_view_model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_study_mvvm/data/repository/xxx/xxx_repository.dart';
+import 'package:flutter_study_mvvm/data/repository/xxx/xxx_repository_impl.dart';
 import 'package:flutter_study_mvvm/ui/xxx/xxx_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -14,10 +16,17 @@ class XXXViewModel extends StateNotifier<AsyncValue<XXXState>> {
     load();
   }
 
-  void load() {
-    state = const AsyncValue.data(
-      XXXState(count: 0),
-    );
+  late final XXXRepository xxxRepository = _ref.read(xxxRepositoryProvider);
+
+  Future<void> load() async {
+    final result = await xxxRepository.fetch();
+    result.when(success: (data) {
+      state = AsyncValue.data(
+        XXXState(count: data),
+      );
+    }, failure: (error) {
+      state = AsyncValue.error(error, StackTrace.current);
+    });
   }
 
   void increment() {
