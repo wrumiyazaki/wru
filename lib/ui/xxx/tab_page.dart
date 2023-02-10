ee256b424f2f46f20492f9dc6860ba6f8ec4916f
krrn0722 <mur0722@outlook.jp>
2023-02-10 14:06:44 +0900

    tab_pageをbottombarを表示させるところにした

--- lib/ui/xxx/tab_page.dart
+++ lib/ui/xxx/tab_page.dart
@@ -6,54 +6,34 @@ import 'package:wru/ui/theme/app_theme.dart';
 import 'package:wru/ui/xxx/xxx_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class TabPage extends HookConsumerWidget {
-  const TabPage({Key? key}) : super(key: key);
+import 'package:auto_route/auto_route.dart';
+import 'package:wru/ui/routes/app_route.gr.dart';
+
+class TabPage extends StatelessWidget {
+  const TabPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final theme = ref.watch(appThemeProvider);
-    final state = ref.watch(xxxViewModelProvider);
-    final viewModel = ref.watch(xxxViewModelProvider.notifier);
-    final l10n = useL10n();
-
-    return state.when(
-      data: (data) {
+  Widget build(BuildContext context) {
+    return AutoTabsRouter(
+      routes: const [HomeRoute(), ExchangeRoute(), FriendRoute()],
+      builder: (context, child, animation) {
+        final tabsRouter = AutoTabsRouter.of(context);
         return Scaffold(
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Assets.img.flutterIcon.image(width: 200),
-                  Text(
-                    l10n.hello,
-                    style: theme.textTheme.h70.bold(),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: theme.appColors.primary,
-                      onPrimary: theme.appColors.onPrimary,
-                    ),
-                    onPressed: viewModel.increment,
-                    child: Text(
-                      data.count.toString(),
-                    ),
-                  ),
-                ],
-              ),
+            body: FadeTransition(
+              opacity: animation,
+              child: child,
             ),
-          ),
-        );
-      },
-      error: (error, stackTrace) => Text(error.toString()),
-      loading: () {
-        return const Scaffold(
-          body: SafeArea(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          ),
-        );
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: tabsRouter.activeIndex,
+              onTap: (index) {
+                tabsRouter.setActiveIndex(index);
+              },
+              items: const [
+                BottomNavigationBarItem(label: 'Tab1', icon: Icon(Icons.check)),
+                BottomNavigationBarItem(label: 'Tab2', icon: Icon(Icons.check)),
+                BottomNavigationBarItem(label: 'Tab3', icon: Icon(Icons.check)),
+              ],
+            ));
       },
     );
   }
