a8a40d38175c4fc9198de772ad192f23155faed9
krrn0722 <mur0722@outlook.jp>
2023-02-18 01:09:31 +0900

    appthemeproviderを使うためにStlをHookCustomerWidgetに変更

--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wru/ui/theme/app_theme.dart';
 
 class ProfileElement {
   final String name;
@@ -16,20 +18,36 @@ final models = [
   ProfileElement('所属', 'bbb'),
 ];
 
-Widget modelToWidget(ProfileElement model) {
-  return Column(children: [
-    Text(model.name),
-    Text(model.text),
-  ]);
-}
-
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends HookConsumerWidget {
   const ProfilePage({super.key});
 
+  //プロフィール１つ分のWidget
+  Widget modelToWidget(ProfileElement model) {
+    return Container(
+      child: Column(children: [
+        Container(
+          child: Text(model.name),
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+        ),
+        Container(
+          child: Text(model.text),
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.fromLTRB(40, 0, 0, 0),
+        ),
+      ]),
+    );
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: ListView.builder(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appThemeProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('プロフィール'),
+        actions: [],
+      ),
+      body: ListView.builder(
         itemCount: models.length,
         itemBuilder: (context, index) => modelToWidget(models[index]),
       ),
