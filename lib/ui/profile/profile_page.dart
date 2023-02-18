41936bd3326500780dcd6b8872f92427c56efc36
krrn0722 <mur0722@outlook.jp>
2023-02-18 12:03:59 +0900

    プロフィール確認画面の配置の完成

--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wru/ui/theme/app_text_theme.dart';
 import 'package:wru/ui/theme/app_theme.dart';
 
 class ProfileElement {
@@ -14,34 +15,58 @@ class ProfileElement {
 }
 
 final models = [
-  ProfileElement('名前', 'aaa'),
-  ProfileElement('所属', 'bbb'),
+  //変更されたら反映させてあげないといけない
+  ProfileElement('名前', '小林 ゆうひ'),
+  ProfileElement('読み方', 'こばやし ユウヒ'),
+  ProfileElement('ユーザーID', '3902189475234'),
+  ProfileElement('生年月日', '7/22'),
+  ProfileElement('電話番号', '9008098800'),
+  ProfileElement('メールアドレス', 'aaafas@fdasf.afas'),
+  ProfileElement('性別', 'aaa'),
+  ProfileElement('所属１', 'aaaaaaaaaaa'),
+  ProfileElement('所属２', 'aaaaaaaaaaa'),
+  ProfileElement('所属３', 'aaaaaaaa'),
+  ProfileElement('追加？', 'aaa'),
+  ProfileElement('追加？', 'aaa'),
+  ProfileElement('追加？', 'aaa'),
 ];
 
 class ProfilePage extends HookConsumerWidget {
   const ProfilePage({super.key});
 
-  //プロフィール１つ分のWidget
-  Widget modelToWidget(ProfileElement model) {
-    return Container(
-      child: Column(children: [
-        Container(
-          child: Text(model.name),
-          alignment: Alignment.centerLeft,
-          padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-        ),
-        Container(
-          child: Text(model.text),
-          alignment: Alignment.centerLeft,
-          padding: EdgeInsets.fromLTRB(40, 0, 0, 0),
-        ),
-      ]),
-    );
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(appThemeProvider);
+
+    //プロフィール１つ分のWidget
+    Widget modelToWidget(ProfileElement model) {
+      return Container(
+        child: Column(children: [
+          Container(
+            child: Text(
+              model.name,
+              style: theme.textTheme.h20.copyWith(color: Colors.grey.shade700),
+            ),
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(30, 20, 0, 5),
+          ),
+          Container(
+            child: Text(
+              model.text,
+              style: theme.textTheme.h40,
+            ),
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+          ),
+          // Divider( //線を引くやつ
+          //   indent: 30,
+          //   endIndent: 30,
+          //   thickness: 1,
+          // )
+        ]),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('プロフィール'),
