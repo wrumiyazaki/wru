f85e1fb7dc92327723b9fc930d4ff8f0da180fe7
krrn0722 <mur0722@outlook.jp>
2023-02-19 20:28:52 +0900

    プロフィールボックスの見た目の完成

--- lib/ui/tabs/home/home_page.dart
+++ lib/ui/tabs/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:wru/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
@@ -13,13 +15,87 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: ElevatedButton(
-            onPressed: () => context.router.push(ProfileRoute()),
-            child: const Text('プロフィールへ')),
-      )),
-    );
+    final theme = ref.watch(appThemeProvider);
+    return SafeArea(
+        child: Column(
+      children: [
+        Container(
+          //プロフィールボックス
+          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+          decoration: BoxDecoration(
+              color: theme.appColors.homeProfileBackground,
+              borderRadius: BorderRadius.circular(10)),
+          child: Row(
+            //テキストとボタンを横に並べる
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                //テキストの集まりコンテナ
+                height: 150,
+                width: 200,
+                padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                child: Column(
+                  //テキストカラム
+                  children: [
+                    const SizedBox(height: 12),
+                    Text(
+                      "小林  ゆうひ",
+                      style: theme.textTheme.h60
+                          .bold()
+                          .copyWith(color: theme.appColors.homeProfileText),
+                    ),
+                    const SizedBox(height: 0),
+                    Text(
+                      "kobayashialyuhifasasad", //英数字22文字まで
+                      style: theme.textTheme.h40
+                          .bold()
+                          .copyWith(color: theme.appColors.homeProfileText),
+                    ),
+                    const SizedBox(height: 7),
+                    Text(
+                      "Exchange : 18",
+                      style: theme.textTheme.h40
+                          .bold()
+                          .copyWith(color: theme.appColors.homeProfileText),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      "userID:1092090",
+                      style: theme.textTheme.h40
+                          .bold()
+                          .copyWith(color: theme.appColors.homeProfileText),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(width: 20),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: theme.appColors.homeProfileText, width: 2)),
+                child: IconButton(
+                  icon: const Icon(Icons.settings),
+                  iconSize: 80,
+                  color: theme.appColors.homeProfileText,
+                  onPressed: () => context.router.push(ProfileRoute()),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(color: Colors.amber),
+          child: Text("aa"),
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+        ),
+        Container(
+          decoration: BoxDecoration(color: Colors.green),
+          child: Text("uu"),
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+        ),
+      ],
+    ));
   }
 }
