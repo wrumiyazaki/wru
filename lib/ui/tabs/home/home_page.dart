4792a3b7fe9fd35763f8568f7dac6ccba5d551db
krrn0722 <mur0722@outlook.jp>
2023-02-19 12:38:05 +0900

    xxx_pageを復活させて、home_pageをプロフィール遷移ボタンだけにした

--- lib/ui/tabs/home/home_page.dart
+++ lib/ui/tabs/home/home_page.dart
@@ -13,53 +13,13 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final theme = ref.watch(appThemeProvider);
-    final state = ref.watch(xxxViewModelProvider);
-    final viewModel = ref.watch(xxxViewModelProvider.notifier);
-    final l10n = useL10n();
-
-    return state.when(
-      data: (data) {
-        return Scaffold(
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Assets.img.flutterIcon.image(width: 200),
-                  Text(
-                    l10n.hello,
-                    style: theme.textTheme.h70.bold(),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: theme.appColors.primary,
-                      onPrimary: theme.appColors.onPrimary,
-                    ),
-                    onPressed: viewModel.increment,
-                    child: Text(
-                      data.count.toString(),
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () => context.router.push(ProfileRoute()),
-                      child: const Text('プロフィールへ'))
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-      error: (error, stackTrace) => Text(error.toString()),
-      loading: () {
-        return const Scaffold(
-          body: SafeArea(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          ),
-        );
-      },
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: ElevatedButton(
+            onPressed: () => context.router.push(ProfileRoute()),
+            child: const Text('プロフィールへ')),
+      )),
     );
   }
 }
