b86e0c74b42a505990143121a5e3fa7ea32e9fd6
krrn0722 <mur0722@outlook.jp>
2023-02-22 04:28:18 +0900

    fix:  変更があったときに出現するボタンが常に表示されるよう修正

--- lib/ui/profile/profile_edit_page.dart
+++ lib/ui/profile/profile_edit_page.dart
@@ -1,14 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wru/ui/profile/profile_view_model.dart';
 import 'package:wru/ui/routes/app_route.gr.dart';
 import 'package:wru/ui/theme/app_theme.dart';
 import 'profile_page.dart';
-
-final textchangedProvider = StateProvider<bool>((ref) {
-  //変化するデータ
-  return false;
-});
 
 class ProfileEditPage extends HookConsumerWidget {
   const ProfileEditPage({super.key});
@@ -16,37 +12,30 @@ class ProfileEditPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(appThemeProvider);
-    final textchanged = ref.watch(textchangedProvider);
 
     //プロフィール編集１つ分のWidget
-    Widget modelToWidget(ProfileElement model) {
+    Widget modelToWidget(index, mapkey) {
       return Container(
         child: Column(children: [
           Container(
-            child: Text(
-              model.name,
-              style: theme.textTheme.h20.copyWith(color: Colors.grey.shade700),
-            ),
             alignment: Alignment.centerLeft,
             padding: EdgeInsets.fromLTRB(30, 20, 0, 5),
+            child: Text(
+              profilePropList[index],
+              style: theme.textTheme.h20.copyWith(color: Colors.grey.shade700),
+            ),
           ),
           Container(
-            child: TextFormField(
-              style: theme.textTheme.h40,
-              controller: TextEditingController(text: model.text),
-              onChanged: (value) {
-                ref.read(textchangedProvider.notifier).state =
-                    true; //文字が変更されたらtrueにする
-              },
-            ),
             alignment: Alignment.centerLeft,
             padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+            child: TextFormField(
+              style: theme.textTheme.h40,
+              controller: TextEditingController(text: getMap[mapkey]),
+              onChanged: (value) {
+                getMap[mapkey] = value;
+              },
+            ),
           ),
-          // Divider( //線を引くやつ
-          //   indent: 30,
-          //   endIndent: 30,
-          //   thickness: 1,
-          // )
         ]),
       );
     }
@@ -54,39 +43,43 @@ class ProfileEditPage extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         //この辺を変更するときはprofile_pageも変更する
-        title: const Text('プロフィール'),
+        title: const Text('プロフィール編集'),
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios_new),
           color: Colors.grey,
           onPressed: () {
-            context.popRoute(
-                ref.read(textchangedProvider.notifier).state = false //falseに戻す
-                );
+            context.popRoute();
           },
         ),
         backgroundColor: theme.appColors.background,
         elevation: 0,
-        actions: textchanged == false
-            ? null
-            : [
-                TextButton(
-                  style: TextButton.styleFrom(),
-                  onPressed: () {
-                    //firebaseに保存する処理がここにくる
-                    ref.read(textchangedProvider.notifier).state =
-                        false; //falseに戻す
-                    context.pushRoute(TabRoute());
-                  },
-                  child: Text(
-                    '保存',
-                    style: theme.textTheme.h40.copyWith(color: Colors.black),
-                  ),
-                )
-              ],
+        actions: [
+          TextButton(
+            style: TextButton.styleFrom(),
+            onPressed: () {
+              // firebaseに保存する処理がここにくる
+              context.pushRoute(TabRoute());
+            },
+            child: Text(
+              '保存',
+              style: theme.textTheme.h40.copyWith(color: Colors.black),
+            ),
+          )
+        ],
       ),
-      body: ListView.builder(
-        itemCount: models.length,
-        itemBuilder: (context, index) => modelToWidget(models[index]),
+      body: ListView(
+        children: [
+          modelToWidget(0, MapKey().name),
+          modelToWidget(1, MapKey().namePhonetic),
+          modelToWidget(2, MapKey().userID),
+          modelToWidget(3, MapKey().birthday),
+          modelToWidget(4, MapKey().telePhoneNumber),
+          modelToWidget(5, MapKey().email),
+          modelToWidget(6, MapKey().gender),
+          modelToWidget(7, MapKey().belonging1),
+          modelToWidget(8, MapKey().belonging2),
+          modelToWidget(9, MapKey().belonging3),
+        ],
       ),
     );
   }
