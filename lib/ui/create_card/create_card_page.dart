f7797c926d21d46ba84c58b9aab87f00913ab9f2
shunsuke-tamura <wwwxx4869@gmail.com>
2023-02-22 20:52:51 +0900

    update: 入力フォームだけスクロールできるように実装

--- lib/ui/create_card/create_card_page.dart
+++ lib/ui/create_card/create_card_page.dart
@@ -14,81 +14,246 @@ class CreateCardPage extends HookConsumerWidget {
     return Scaffold(
       backgroundColor: theme.appColors.background,
       body: SafeArea(
-        child: AspectRatio(
-          aspectRatio: Constants.cardAspectRatio,
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            width: double.infinity,
-            height: 300,
-            child: Card(
-              color: theme.appColors.surface,
-              shadowColor: theme.appColors.primary,
-              elevation: 1,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        Text('宮崎大学', style: theme.textTheme.h30),
-                        Text('理工学部', style: theme.textTheme.h30),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text('情報コース知能情報専攻', style: theme.textTheme.h30),
-                        Text('3', style: theme.textTheme.h30),
-                        Text('年', style: theme.textTheme.h30),
-                      ],
-                    ),
-                    const Expanded(
-                      child: SizedBox(
-                        height: double.infinity,
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 10),
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              AspectRatio(
+                aspectRatio: Constants.cardAspectRatio,
+                child: SizedBox(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Card(
+                    color: theme.appColors.surface,
+                    shadowColor: theme.appColors.primary,
+                    elevation: 1,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
                       child: Column(
                         children: [
+                          const SizedBox(
+                            height: 10,
+                          ),
                           Row(
                             children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('田村駿典',
-                                      style: theme.textTheme.h50.bold()),
-                                  Text('Shunsuke Tamura',
-                                      style: theme.textTheme.h10.dense()),
-                                ],
-                              )
+                              Text('宮崎大学', style: theme.textTheme.h30),
+                              Text('理工学部', style: theme.textTheme.h30),
                             ],
                           ),
                           Row(
                             children: [
-                              Text('宮崎県', style: theme.textTheme.h20),
-                              Text('佐土原高校', style: theme.textTheme.h20),
-                              Text('出身', style: theme.textTheme.h20),
+                              Text('情報コース知能情報専攻', style: theme.textTheme.h30),
+                              Text('3', style: theme.textTheme.h30),
+                              Text('年', style: theme.textTheme.h30),
                             ],
                           ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'バドミントン歴11年\n好きなバンド Mrs. GREEN APPLE\n好きなアニメ 進撃の巨人',
-                                style: theme.textTheme.h10.dense(),
-                              )
-                            ],
+                          const Expanded(
+                            child: SizedBox(
+                              height: double.infinity,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text('田村駿典',
+                                            style: theme.textTheme.h50.bold()),
+                                        Text('Shunsuke Tamura',
+                                            style: theme.textTheme.h10.dense()),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text('宮崎県', style: theme.textTheme.h20),
+                                    Text('佐土原高校', style: theme.textTheme.h20),
+                                    Text('出身', style: theme.textTheme.h20),
+                                  ],
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'バドミントン歴11年\n好きなバンド Mrs. GREEN APPLE\n好きなアニメ 進撃の巨人',
+                                      style: theme.textTheme.h10.dense(),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
+              Expanded(
+                child: Scrollbar(
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '所属大学',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: theme.appColors.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
