9a9cbda18c114f6c3bf5876be674c40cf36ad9e9
shunsuke-tamura <wwwxx4869@gmail.com>
2023-02-23 17:26:23 +0900

    update: テンプレート選択画面の見た目作成

--- lib/ui/create_card/select_template/select_template.dart
+++ lib/ui/create_card/select_template/select_template.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wru/ui/create_card/templates/normal_card/normal_card_page.dart';
 
 class SelectTemplatePage extends HookConsumerWidget {
   const SelectTemplatePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
+    final templates = [
+      const NormalCardPage(),
+      const NormalCardPage(),
+      const NormalCardPage(),
+      const NormalCardPage(),
+      const NormalCardPage()
+    ];
+
+    return Scaffold(
       body: SafeArea(
-        child: Text('select template'),
+        child: Scrollbar(
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            padding: const EdgeInsets.only(
+              left: 8,
+              right: 8,
+            ),
+            child: ListView.separated(
+              itemCount: templates.length,
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 8,
+              ),
+              itemBuilder: (context, index) => InkWell(
+                onTap: () => print('selected'),
+                child: templates[index],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
