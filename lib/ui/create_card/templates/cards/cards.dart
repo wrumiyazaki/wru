d77f35e961b9a8525d3f3b5059b4ee91a5f817ed
shunsuke-tamura <wwwxx4869@gmail.com>
2023-02-24 21:03:14 +0900

    add: testCardTemplateを追加

--- lib/ui/create_card/templates/cards/cards.dart
+++ lib/ui/create_card/templates/cards/cards.dart
@@ -1,5 +1,7 @@
 import 'package:wru/ui/create_card/templates/cards/normal_card/normal_card_page.dart';
+import 'package:wru/ui/create_card/templates/cards/test_card/normal_card_page.dart';
 
 class CardTemplate {
   static const normalTemplate = NormalCardPage();
+  static const testTemplate = TestCardPage();
 }
