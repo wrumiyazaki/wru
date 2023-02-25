1a1c95ff7f7eb339ec13980bb67c818d6ddedc92
krrn0722 <mur0722@outlook.jp>
2023-02-25 16:06:28 +0900

    add: QrDisplayPageの実装

---
+++ lib/ui/tabs/exchange/qr_display_page.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:wru/ui/routes/app_route.gr.dart';
+
+class QrDisplayPage extends StatelessWidget {
+  const QrDisplayPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
