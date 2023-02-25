9f1b74c6f562c50f6b2d44d43711083a934e19ea
krrn0722 <mur0722@outlook.jp>
2023-02-25 17:08:22 +0900

    upgrade: 表示とスキャン画面が互いに遷移できるよう実装

--- lib/ui/tabs/exchange/qr_display_page.dart
+++ lib/ui/tabs/exchange/qr_display_page.dart
@@ -2,13 +2,21 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wru/ui/routes/app_route.gr.dart';
 
-class QrDisplayPage extends StatelessWidget {
+class QrDisplayPage extends HookConsumerWidget {
   const QrDisplayPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+          onPressed: () {
+            context.pushRoute(const QrScanRoute());
+          },
+          child: Text('Display')),
+    );
   }
 }
--- lib/ui/tabs/exchange/qr_scan_page.dart
+++ lib/ui/tabs/exchange/qr_scan_page.dart
@@ -1,13 +1,21 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wru/ui/routes/app_route.gr.dart';
+import 'package:wru/ui/tabs/exchange/qr_display_page.dart';
 
-class QrScanPage extends StatelessWidget {
+class QrScanPage extends HookConsumerWidget {
   const QrScanPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       padding: const EdgeInsets.all(8.0),
-      child: const Text('Exchange'),
+      child: ElevatedButton(
+          onPressed: () async {
+            context.router.popAndPush(QrDisplayRoute());
+          },
+          child: Text('Scan')),
     );
   }
 }
