0514c0354c224238c9548274f081e36cbe2c58d8
shunsuke-tamura <wwwxx4869@gmail.com>
2023-02-27 20:33:37 +0900

    update: inputItemsのinitialValueの定義を追加

--- lib/ui/create_card/templates/input_items/input_items.dart
+++ lib/ui/create_card/templates/input_items/input_items.dart
@@ -15,6 +15,16 @@ class InputItemsTemplate {
     InputItem(label: '名前(ローマ字表記)'),
     InputItem(label: '出身/趣味など', isMultiLine: true)
   ];
+  static final normalInputInitialValue = [
+    '〇〇大学',
+    '△△学部',
+    '〇〇コース〇〇専攻',
+    'X',
+    '山田 太郎',
+    'Taro Yamada',
+    '〇〇県 〇〇高校出身\nサッカー歴11年\n好きな歌 国家'
+  ];
+
   static final testInputItems = [
     InputItem(label: '名前'),
     InputItem(label: '名前(ローマ字表記)'),
@@ -23,5 +33,14 @@ class InputItemsTemplate {
     InputItem(label: '学科/専攻/コース'),
     InputItem(label: '学年'),
     InputItem(label: '出身/趣味など', isMultiLine: true)
+  ];
+  static final testInputInitialValue = [
+    '山田 太郎',
+    'Taro Yamada',
+    '〇〇大学',
+    '△△学部',
+    '〇〇コース〇〇専攻',
+    'X',
+    '〇〇県 〇〇高校出身\nサッカー歴11年\n好きな歌 国家'
   ];
 }
