2c833c7809f1e88005b6e102c7975a1f3cdeceef
krrn0722 <mur0722@outlook.jp>
2023-02-27 17:23:06 +0900

    update: 写真をタップすると別の画像を表示するよう実装

--- lib/ui/tabs/exchange/reicive_page.dart
+++ lib/ui/tabs/exchange/reicive_page.dart
@@ -19,13 +19,15 @@ class RecievePage extends HookConsumerWidget {
     final theme = ref.watch(appThemeProvider);
     final l10n = useL10n();
     final controllerstate = ref.watch(qrCodeProvider);
+    final imagestate = ref.watch(imageprovider);
+    final imagestatenotifier = ref.read(imageprovider.notifier);
     String memo = '';
 
     return SingleChildScrollView(
       child: Column(
         children: [
           Container(
-            padding: EdgeInsets.fromLTRB(10, 100, 10, 10),
+            padding: EdgeInsets.fromLTRB(10, 120, 10, 0),
             child: Text(
               style: theme.textTheme.h50
                   .copyWith(color: theme.appColors.receivePageText),
@@ -35,14 +37,29 @@ class RecievePage extends HookConsumerWidget {
             // alignment: Alignment.center,
           ),
           Container(
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: Image.network(
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
-              fit: BoxFit.contain,
+            height: 250,
+            width: 413.636,
+            padding: EdgeInsets.fromLTRB(10, 40, 10, 0),
+            child: GestureDetector(
+              onTap: () {
+                imagestatenotifier.state = !imagestate;
+                print(imagestate);
+              },
+              child: imagestate
+                  ? Image.asset(
+                      //firebase
+                      'assets/img/namecard-sample.png',
+                      fit: BoxFit.contain,
+                    )
+                  : Image.network(
+                      //firebase
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
+                      fit: BoxFit.contain,
+                    ),
             ),
           ),
           Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+            padding: EdgeInsets.fromLTRB(20, 40, 20, 10),
             child: TextField(
               keyboardType: TextInputType.multiline,
               maxLines: null,
