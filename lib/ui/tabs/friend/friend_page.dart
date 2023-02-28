72c8d0dbc0287c14b9799257ef735f316878d2b5
krrn0722 <mur0722@outlook.jp>
2023-02-28 23:21:57 +0900

    update: メモが表示されるようにした

--- lib/ui/tabs/friend/friend_page.dart
+++ lib/ui/tabs/friend/friend_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wru/ui/hooks/use_l10n.dart';
 import 'package:wru/ui/tabs/friend/friend_view_model.dart';
+import 'package:wru/ui/theme/app_theme.dart';
 
 class FriendPage extends HookConsumerWidget {
   FriendPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = useL10n();
+    final theme = ref.watch(appThemeProvider);
     final boolsstate = ref.watch(boolsprovider);
     final boolsstatenotifier = ref.read(boolsprovider.notifier);
 
@@ -28,10 +32,48 @@ class FriendPage extends HookConsumerWidget {
                     FriendViewModel().nameCardImage[index],
                     fit: BoxFit.contain,
                   )
-                : Image.network(
-                    //firebase
-                    FriendViewModel().portraitImage[index],
-                    fit: BoxFit.contain,
+                : Row(
+                    children: [
+                      Container(
+                        height: 200,
+                        width: 150,
+                        child: Image.network(
+                          //firebase
+                          FriendViewModel().portraitImage[index],
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+                                width: double.infinity,
+                                child: SingleChildScrollView(
+                                  child: Text(
+                                    FriendViewModel().memo[index],
+                                    style: theme.textTheme.h40,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            ElevatedButton(
+                              style: TextButton.styleFrom(
+                                  minimumSize: Size(180, 20),
+                                  backgroundColor: Colors.grey),
+                              onPressed: () {
+                                //firebaseにメモを保存
+                              },
+                              child: Text(l10n.edit),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
           ),
         ),
--- lib/ui/tabs/friend/friend_view_model.dart
+++ lib/ui/tabs/friend/friend_view_model.dart
@@ -20,17 +20,29 @@ class FriendViewModel extends StatelessWidget {
 
 //firebaseから取得
   final List<String> portraitImage = [
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
+    'https://media.istockphoto.com/id/946516860/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E9%9D%92%E3%81%84%E7%A9%BA%E3%81%A8%E6%B5%9C%E6%9D%BE%E7%94%BA%E6%97%A5%E6%9C%AC%E3%81%A7%E6%99%B4%E3%82%8C%E3%81%9F%E6%97%A5%E3%81%AE%E4%B8%8B%E3%81%A7%E3%81%AE%E6%9D%B1%E4%BA%AC%E3%82%BF%E3%83%AF%E3%83%BC.jpg?s=2048x2048&w=is&k=20&c=B893-I7U1DOVp-ePN0fddjo_4Zk05ySED452ZaHbVaw=',
+    'https://media.istockphoto.com/id/946516860/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E9%9D%92%E3%81%84%E7%A9%BA%E3%81%A8%E6%B5%9C%E6%9D%BE%E7%94%BA%E6%97%A5%E6%9C%AC%E3%81%A7%E6%99%B4%E3%82%8C%E3%81%9F%E6%97%A5%E3%81%AE%E4%B8%8B%E3%81%A7%E3%81%AE%E6%9D%B1%E4%BA%AC%E3%82%BF%E3%83%AF%E3%83%BC.jpg?s=2048x2048&w=is&k=20&c=B893-I7U1DOVp-ePN0fddjo_4Zk05ySED452ZaHbVaw=',
+    'https://media.istockphoto.com/id/946516860/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E9%9D%92%E3%81%84%E7%A9%BA%E3%81%A8%E6%B5%9C%E6%9D%BE%E7%94%BA%E6%97%A5%E6%9C%AC%E3%81%A7%E6%99%B4%E3%82%8C%E3%81%9F%E6%97%A5%E3%81%AE%E4%B8%8B%E3%81%A7%E3%81%AE%E6%9D%B1%E4%BA%AC%E3%82%BF%E3%83%AF%E3%83%BC.jpg?s=2048x2048&w=is&k=20&c=B893-I7U1DOVp-ePN0fddjo_4Zk05ySED452ZaHbVaw=',
+    'https://media.istockphoto.com/id/946516860/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E9%9D%92%E3%81%84%E7%A9%BA%E3%81%A8%E6%B5%9C%E6%9D%BE%E7%94%BA%E6%97%A5%E6%9C%AC%E3%81%A7%E6%99%B4%E3%82%8C%E3%81%9F%E6%97%A5%E3%81%AE%E4%B8%8B%E3%81%A7%E3%81%AE%E6%9D%B1%E4%BA%AC%E3%82%BF%E3%83%AF%E3%83%BC.jpg?s=2048x2048&w=is&k=20&c=B893-I7U1DOVp-ePN0fddjo_4Zk05ySED452ZaHbVaw=',
+    'https://media.istockphoto.com/id/946516860/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E9%9D%92%E3%81%84%E7%A9%BA%E3%81%A8%E6%B5%9C%E6%9D%BE%E7%94%BA%E6%97%A5%E6%9C%AC%E3%81%A7%E6%99%B4%E3%82%8C%E3%81%9F%E6%97%A5%E3%81%AE%E4%B8%8B%E3%81%A7%E3%81%AE%E6%9D%B1%E4%BA%AC%E3%82%BF%E3%83%AF%E3%83%BC.jpg?s=2048x2048&w=is&k=20&c=B893-I7U1DOVp-ePN0fddjo_4Zk05ySED452ZaHbVaw=',
+  ];
+
+  final List<String> memo = [
+    'aaa',
+    'iii',
+    'uuuoooぁｋｓｊｄｆぁｋｊふぁ；ｓｊ\n\naa\nfasf\n\n\nddd\nddd',
+    'eee',
+    'ooo',
   ];
 
 //firebaseから要素数を取得
   late final listlength = nameCardImage.length;
 
   late final List<bool> boolList = List.filled(listlength, true);
+
+  void memoSave(String text) {
+    print(text);
+  }
 
   @override
   Widget build(BuildContext context) {
