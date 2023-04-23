8d1c130df15b4a7db21aafefe122272d5999f40c
krrn0722 <mur0722@outlook.jp>
2023-04-24 00:29:44 +0900

    add: uidをプロバイダーで管理する

--- lib/data/repository/profile/profile_repository_impl.dart
+++ lib/data/repository/profile/profile_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wru/data/provider/firebase_firestore_provider.dart';
+import 'package:wru/data/provider/uid_provider.dart';
 import 'package:wru/data/repository/profile/profile_repository.dart';
 import 'package:wru/ui/profile/profile_state.dart';
 import 'package:wru/ui/profile/profile_view_model.dart';
@@ -12,6 +13,8 @@ class ProfileRepositoryImpl implements ProfileRepository {
   final Ref _ref;
   final String usersCollection = 'users';
   final String profileField = 'profile';
+  //uidを取得
+  late String profileUid = _ref.watch(uidProvider);
   late final FirebaseFirestore db = _ref.read(firebaseFirestoreProvider);
   late final DocumentReference profileRef =
       db.collection(usersCollection).doc(profileUid);
@@ -20,12 +23,15 @@ class ProfileRepositoryImpl implements ProfileRepository {
   Future<Map> fetchProfileMap() async {
     Map? documentMap;
     Map? profileMap;
+    //取得したマップがヌルの場合とりあえずからのマップを返してプロフィールを埋めた時につくってもらう
+    Map tentativeMap = {};
     DocumentSnapshot? snapshot = await profileRef.get();
     documentMap = snapshot.data() as Map;
     if (documentMap[profileField] != null) {
       profileMap = documentMap[profileField];
+      return Future<Map>.value(profileMap);
     }
-    return Future<Map>.value(profileMap);
+    return tentativeMap;
   }
 
   void saveProfileMap(Map map, int index) {
