4a2258cc50401969663c7a81113773e1d379e490
krrn0722 <mur0722@outlook.jp>
2023-06-04 20:07:22 +0900

    add: uidProviderをローカルに保存したものに変更

--- lib/data/provider/uid_provider.dart
+++ lib/data/provider/uid_provider.dart
@@ -1,6 +1,31 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final initUidProvider = StateNotifierProvider((ref) => InitUidNotifier(ref));
+
+final fetchUidProvider = FutureProvider((ref) async {
+  const key = "uid_key";
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  //uidProviderに管理してもらう
+  final uid = pref.getString(key) ?? '';
+  ref.watch(uidProvider.notifier).state = uid;
+  return uid;
+});
+
+class InitUidNotifier extends StateNotifier {
+  InitUidNotifier(this._ref) : super('');
+  final Ref _ref;
+  final key = "uid_key";
+
+  //uidを保存する
+  Future<void> saveUid(String uid) async {
+    //uidProviderに管理してもらう
+    _ref.watch(uidProvider.notifier).state = uid;
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString(key, uid);
+    print(_ref.watch(uidProvider));
+  }
+}
 
 //uidのプロバイダー
-//#TODO
-final uidProvider =
-    StateProvider<String>((ref) => 'EKwJmH5SzfdmEmSxcTeIKcBkLW42');
+final uidProvider = StateProvider((_) => '');
